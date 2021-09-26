e33e59f3a1d7100595b6fd6791dccaee59bd51b1
Andr-Shch <74563519+cybertrip-glitch@users.noreply.github.com>
2021-09-26 20:30:57 +0300

    readme

--- src/App.js
+++ src/App.js
@@ -2,11 +2,30 @@
 import { BrowserRouter } from 'react-router-dom';
 import ChatInterface from './components/ChatInterface';
 import { GlobalStyle } from './GlobalStyles';
+import FacebookLogin from 'react-facebook-login';
+
+  //  onClick={componentClicked}
+  //  callback={responseFacebook} 
 
 function App() {
+
+  const responseFacebook=(response)=>{
+    console.log(response);
+  }
+
+
+
   return (
     <>
+     
      <BrowserRouter>
+     <FacebookLogin
+    appId="234597485389849"
+    autoLoad={true}
+    fields="name,email,picture"
+    callback={responseFacebook} 
+
+  />
        <ChatInterface/>
        <GlobalStyle/>
     
